790b68cd0fc97d14e7951be30db15f33eaa2f699
ericung <ungericwei@gmail.com>
2022-09-29 19:26:16 -0500

    send email

--- IdentityApp/Pages/Account/Register.cshtml.cs
+++ IdentityApp/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Mail;
 
 namespace IdentityApp.Pages.Account
 {
@@ -40,8 +42,24 @@ namespace IdentityApp.Pages.Account
             if (result.Succeeded)
             {
                 var confirmationToken = await UserManager.GenerateEmailConfirmationTokenAsync(user);
-                return Redirect(Url.PageLink(pageName: "/Account/ConfirmEmail",
-                    values: new { userId = user.Id, token = confirmationToken }));
+                var confirmationLink = Url.PageLink(pageName: "/Account/ConfirmEmail",
+                    values: new { userId = user.Id, token = confirmationToken });
+
+                var message = new MailMessage("ungericwei@gmail.com",
+                    user.Email,
+                    "Please confirm your email",
+                    $"Please click on this link to confirm your email: {confirmationLink}");
+
+                using (var emailClient = new SmtpClient("smtp-relay.sendinblue.com", 587))
+                {
+                    emailClient.Credentials = new NetworkCredential(
+                        "ungericwei@gmail.com",
+                        "--");
+
+                    await emailClient.SendMailAsync(message);
+                }
+
+                return RedirectToPage("/Account/Login");
             }
             else
             {
