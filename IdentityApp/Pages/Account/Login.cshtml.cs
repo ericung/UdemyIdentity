afb71c431b1726bdef8e96b7ab310d5b94484740
ericung <ungericwei@gmail.com>
2022-09-30 17:50:29 -0500

    Logout

--- IdentityApp/Pages/Account/Login.cshtml.cs
+++ IdentityApp/Pages/Account/Login.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 
-namespace Identity.Pages.Account
+namespace IdentityApp.Pages.Account
 {
     public class LoginModel : PageModel
     {
